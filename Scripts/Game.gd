c0555c06ba9965d76aa69c119f7c9d27c2753e97
Yann Burrett <yann.let@gmail.com>
2018-09-29 13:38:06 +0100

    Ball now freezes on goal, then unfreezes on reset

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -3,17 +3,18 @@ extends Spatial
 var Player1_score = 0
 var Player2_score = 0
 
+onready var Ball = get_tree().get_root().find_node("Ball", true, false)
 export var target_score = 5
 
 func _ready():
 	reset_pitch()
-	pass
 
 
 func reset_pitch():
-	var Ball = get_tree().get_root().find_node("Ball", true, false)
 	var BallStart = get_tree().get_root().find_node("BallStart", true, false)
 	Ball.translation = BallStart.translation
+	Ball.axis_lock_linear_x = false
+	Ball.axis_lock_linear_z = false
 	get_tree().call_group("players", "reset")
 
 
@@ -22,13 +23,19 @@ func freeze_players():
 
 
 func _on_GoalDetector_body_entered(body, goal_id):
+	var Particles1= get_tree().get_root().find_node("Particles1", true, false)
+	var Particles2= get_tree().get_root().find_node("Particles2", true, false)
+	
+	Ball.axis_lock_linear_x = true
+	Ball.axis_lock_linear_z = true
+	
 	update_score(goal_id)
 	freeze_players()
 	$Timer.start()
 	if goal_id == 1:
-		$Particles1.emitting = true
+		Particles1.emitting = true
 	else:
-		$Particles2.emitting = true
+		Particles2.emitting = true
 
 
 func _on_Timer_timeout():
@@ -37,6 +44,7 @@ func _on_Timer_timeout():
 
 func update_score(goal_id):
 	var new_score
+	
 	if goal_id == 1:
 		Player1_score +=1
 		new_score = Player1_score
