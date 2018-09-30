f92b5da6f395e505c5fd39db8be1b8ee2145ef45
Yann Burrett <yann.let@gmail.com>
2018-09-30 10:06:59 +0100

    Added simple GUI graphics and a basic background wall

--- Scripts/GUI.gd
+++ Scripts/GUI.gd
@@ -2,9 +2,9 @@ extends CanvasLayer
 
 func update_score(player, score):
 	if player == 1:
-		$HBoxContainer/CenterContainer/Player1Score.text = str(score)
+		$NinePatchRect/HBoxContainer/CenterContainer/Player1Score.text = str(score)
 	else:
-		$HBoxContainer/CenterContainer2/Player2Score.text = str(score)
+		$NinePatchRect/HBoxContainer/CenterContainer2/Player2Score.text = str(score)
 
 
 func end_game(player):
