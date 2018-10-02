9c0faf59a38bc37e413ec4c8bdfe323ec3b9a90a
Yann Burrett <yann.let@gmail.com>
2018-10-02 10:12:43 +0100

    fixed the polished code so that it wasn't just polished but functional

--- Scripts/FollowSpot.gd
+++ Scripts/FollowSpot.gd
@@ -9,12 +9,12 @@ func reset_pitch():
 
 
 func goal(player):
-	var Player1 = get_tree().get_root().find_node("Player1")
-	var Player2 = get_tree().get_root().find_node("Player2")
+	var Player1 = get_tree().get_root().find_node("Player1", true, false)
+	var Player2 = get_tree().get_root().find_node("Player2", true, false)
 
 	if player == 1:
-		look_at(Player1.translation, Vector3(0,1,0)
+		look_at(Player1.translation, Vector3(0,1,0))
 	else:
-		look_at(Player2.translation, Vector3(0,1,0)
+		look_at(Player2.translation, Vector3(0,1,0))
 		
 	show()
